6ba2b23964289dca568153775aa1f3cb84fe2296
alanUno <alan_borin@unochapeco.edu.br>
2023-03-14 20:26:38 -0300

    rota de autenticação api criada

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const jwt = require('jsonwebtoken'); //importa a biblioteca de token JWT
+const usuarios = require('./usuarios.json');
 
 const app = express();
 
@@ -29,6 +31,20 @@ app.use('/produtos', produtosRoutes);
 
 /*const carrosRoutes = require('./routes/carros'); //require do arquivo de rotas dos carros
 app.use('/carros', carrosRoutes); //definição do arquivo de rotas dos carros*/
+
+
+app.post('/auth', (req, res) => {
+   const { email, senha } = req.body; //obtém o email e senha do corpo da requisição
+   const usuario = usuarios.find(u => u.email === email && u.senha === senha); //procura pelo usuário com email e senha informados
+ 
+   if (!usuario) {
+     return res.status(401).send('Usuário não encontrado ou senha inválida'); //retorna erro de autenticação se usuário não for encontrado
+   }
+ 
+   const token = jwt.sign({ email }, 'chave-secreta-do-token'); //gera um token JWT com o email do usuário
+ 
+   res.send({ token }); //retorna o token gerado
+ });
 
 app.listen(3000, () => {
    console.log("Aplicacao rodando!");
