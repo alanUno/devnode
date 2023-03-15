4b304d051bb161bd6248023aeeecc04d2bd94fb8
alanUno <alan_borin@unochapeco.edu.br>
2023-03-14 23:39:44 -0300

    ajustado consulta do token em user do banco

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const jwt = require('jsonwebtoken'); //importa a biblioteca de token JWT
-const usuarios = require('./models/usuario');
+const jwt = require('jsonwebtoken');
+const usuarioModel = require('./models/usuario'); // importa o modelo do usuário
 
 const app = express();
 
-app.use(bodyParser.json()); //usado para a aplicação trabalhar/receber os dados em JSON
+app.use(bodyParser.json());
 
 const DB = require('./setup/DB');
 DB.connect();
@@ -14,7 +14,6 @@ app.use('/teste', express.static('public'));
 
 app.set('view engine', 'ejs');
 
-
 app.get('/home', (req, res) => {
    res.render('index.ejs', {
       aluno: "Alan",
@@ -22,32 +21,27 @@ app.get('/home', (req, res) => {
    })
 });
 
-const pedidosRoutes = require('./routes/pedidos'); 
-app.use('/pedidos', pedidosRoutes); 
+const pedidosRoutes = require('./routes/pedidos');
+app.use('/pedidos', pedidosRoutes);
 
-const produtosRoutes = require('./routes/produtos'); 
-app.use('/produtos', produtosRoutes); 
+const produtosRoutes = require('./routes/produtos');
+app.use('/produtos', produtosRoutes);
 
-const usuariosRoutes = require('./routes/usuarios'); 
+const usuariosRoutes = require('./routes/usuarios');
 app.use('/usuarios', usuariosRoutes);
 
+app.post('/auth', async (req, res) => { // adiciona o modificador async para poder usar await
+   const { email, senha } = req.body;
+   const usuario = await usuarioModel.findOne({ email, senha }); // busca o usuário utilizando o modelo do Mongoose e o método findOne()
 
-/*const carrosRoutes = require('./routes/carros'); //require do arquivo de rotas dos carros
-app.use('/carros', carrosRoutes); //definição do arquivo de rotas dos carros*/
-
-
-app.post('/auth', (req, res) => {
-   const { email, senha } = req.body; //obtém o email e senha do corpo da requisição
-   const usuario = usuarios.find(u => u.email === email && u.senha === senha); //procura pelo usuário com email e senha informados
- 
    if (!usuario) {
-     return res.status(401).send('Usuário não encontrado ou senha inválida'); //retorna erro de autenticação se usuário não for encontrado
+     return res.status(401).send('Usuário não encontrado ou senha inválida');
    }
- 
-   const token = jwt.sign({ email }, 'chave-secreta-do-token'); //gera um token JWT com o email do usuário
- 
-   res.send({ token }); //retorna o token gerado
- });
+
+   const token = jwt.sign({ email }, 'chave-secreta-do-token');
+
+   res.send({ token });
+});
 
 app.listen(3000, () => {
    console.log("Aplicacao rodando!");
