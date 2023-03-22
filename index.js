ec44315b99b1bc42ff63794675941a1c8218b268
alanUno <alan_borin@unochapeco.edu.br>
2023-03-22 20:17:29 -0300

    comments index

--- index.js
+++ index.js
@@ -2,30 +2,39 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const jwt = require('jsonwebtoken');
 const usuarioModel = require('./models/usuario');
+const pedidosRoutes = require('./routes/pedidos');
+const produtosRoutes = require('./routes/produtos');
+const usuariosRoutes = require('./routes/usuarios');
+const populateRoutes = require("./routes/populate");
+const quantidadeRoutes = require("./routes/quantidade");
+const DB = require('./setup/DB');
 
 const app = express();
 
+// middleware
 app.use(bodyParser.json());
 
-const DB = require('./setup/DB');
-DB.connect();
-
+// static files
 app.use('/teste', express.static('public'));
 
+// view engine
 app.set('view engine', 'ejs');
 
-const pedidosRoutes = require('./routes/pedidos');
+// database connection
+DB.connect();
+
+// routes
 app.use('/pedidos', pedidosRoutes);
-
-const produtosRoutes = require('./routes/produtos');
 app.use('/produtos', produtosRoutes);
-
-const usuariosRoutes = require('./routes/usuarios');
 app.use('/usuarios', usuariosRoutes);
+app.use("/populate", populateRoutes);
+app.use("/quantidade", quantidadeRoutes);
 
+// authentication route
 app.post('/auth', async (req, res) => { 
    const { email, senha } = req.body;
    const usuario = await usuarioModel.findOne({ email, senha });
+   
    if (!usuario) {
      return res.status(401).send('Usuário não encontrado ou senha inválida');
    }
@@ -35,12 +44,7 @@ app.post('/auth', async (req, res) => {
    res.send({ token });
 });
 
-const populateRoutes = require("./routes/populate");
-app.use("/populate", populateRoutes);
-
-const quantidadeRoutes = require("./routes/quantidade");
-app.use("/quantidade", quantidadeRoutes);
-
+// server listening
 app.listen(3000, () => {
    console.log("Aplicacao rodando!");
 });
